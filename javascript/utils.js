c512d472d76964e9a5f66f43c1bb4c6aac16a9df
Bram Adams <3282661+bramses@users.noreply.github.com>
2021-12-21 19:01:35 -0500

    utils

--- javascript/utils.js
+++ javascript/utils.js
@@ -5,6 +5,10 @@ const _ = require('lodash');
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY;
 const openai = new OpenAI(OPENAI_API_KEY);
 
+/**
+ * Create a completion from a prompt.
+ * @returns {Promise<String>} trimmed text from GPT call
+ */
 const createCompletion = async ({ engine='davinci-instruct-beta-v3', prompt = '', temperature = 0.0, maxTokens = 64, topP = 1.0, presencePenalty = 0.0, frequencyPenalty = 0.0, bestOf = 1, n = 1, stop = null, debug = false } = {}) => {
     try {
         if (debug) console.log(`createCompletion({ engine: ${engine}, prompt: ${prompt}, temperature: ${temperature}, maxTokens: ${maxTokens}, topP: ${topP}, presencePenalty: ${presencePenalty}, frequencyPenalty: ${frequencyPenalty}, bestOf: ${bestOf}, n: ${n}, stop: ${stop} })`);
